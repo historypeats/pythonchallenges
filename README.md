0b37de76d754bfac385191a2e06dad62653cc214
historypeats <iam.historypeats@gmail.com>
2015-02-17 16:34:16 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Python Challenges
+These are the solutions for the python challenges found here: http://www.pythonchallenge.com/
---
+++ chal0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# Challenge URL: http://www.pythonchallenge.com/pc/def/0.html
+NEXT_URL = 'http://www.pythonchallenge.com/pc/def/PLACEHOLDER.html'
+
+
+def main():
+	''' main '''
+	solution = NEXT_URL.replace('PLACEHOLDER', str(2**38))
+	print 'Solution: {0}'.format(solution)
+
+if __name__ == '__main__':
+	main()
